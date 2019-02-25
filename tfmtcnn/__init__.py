cf2942d4c6b9f3f9d6d2db180433554f9844ae6b
korus <kosarev@smedx.com>
2019-02-25 19:12:01 +0300

    function packagedir added

--- tfmtcnn/__init__.py
+++ tfmtcnn/__init__.py
@@ -1,3 +1,11 @@
 # coding:utf-8
 __author__ = 'Ruslan N. Kosarev'
 __version__ = "0.0.1"
+
+
+import os
+import pathlib as plib
+
+
+def packagedir():
+    return plib.Path(os.path.realpath(__file__)).parent
