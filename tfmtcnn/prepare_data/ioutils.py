675ede137acf77bf800511882d312205b58ba608
korus <kosarev@smedx.com>
2019-03-03 16:43:31 +0300

    refactoring laoder

--- tfmtcnn/prepare_data/ioutils.py
+++ tfmtcnn/prepare_data/ioutils.py
@@ -31,32 +31,33 @@ class ImageLoader:
         return self
 
     def __init__(self, data, prefix=None, display=100):
-        self.counter = -1
+        self.counter = 0
         self.start_time = time.time()
         self.data = data
         self.display = display
         self.size = len(data)
         self.prefix = str(prefix)
 
-    def __next__(self):
-        self.counter += 1
+        print('Loader <{}> is initialized, number of images {}'.format(self.__class__.__name__, self.size))
 
+    def __next__(self):
         if self.counter < self.size:
-            if self.counter % self.display == 0:
+            if (self.counter + 1) % self.display == 0:
                 elapsed_time = (time.time() - self.start_time) / self.display
                 print('\rnumber of processed images {}/{}, {:.5f} seconds per image'.
-                      format(self.counter, self.size, elapsed_time), end='')
+                      format(self.counter+1, self.size, elapsed_time), end='')
                 self.start_time = time.time()
 
             image = read_image(self.data[self.counter], prefix=self.prefix)
 
+            self.counter += 1
             return image
         else:
             print('\rnumber of processed images {}'.format(self.size))
             raise StopIteration
 
     def reset(self):
-        self.counter = -1
+        self.counter = 0
         return self
 
 
@@ -93,48 +94,5 @@ class ImageLoaderWithPath:
             raise StopIteration
 
     def reset(self):
-        self.counter = -1
+        self.counter = 0
         return self
-
-
-def read_annotation(filename):
-
-    if not filename.exists():
-        raise IOError('file {} does not exist'.format(filename))
-    data = dict()
-    images = []
-    bboxes = []
-    labelfile = open(str(filename), 'r')
-
-    while True:
-        # image path
-        imagepath = labelfile.readline().strip('\n')
-        if not imagepath:
-            break
-        # imagepath = base_dir + '/WIDER_train/images/' + imagepath
-        images.append(imagepath)
-        # face numbers
-        nums = labelfile.readline().strip('\n')
-        # im = cv2.imread(imagepath)
-        # h, w, c = im.shape
-        one_image_bboxes = []
-        for i in range(int(nums)):
-            # text = ''
-            # text = text + imagepath
-            bb_info = labelfile.readline().strip('\n').split(' ')
-            # only need x, y, w, h
-            face_box = [float(bb_info[i]) for i in range(4)]
-            # text = text + ' ' + str(face_box[0] / w) + ' ' + str(face_box[1] / h)
-            xmin = face_box[0]
-            ymin = face_box[1]
-            xmax = xmin + face_box[2]
-            ymax = ymin + face_box[3]
-            # text = text + ' ' + str(xmax / w) + ' ' + str(ymax / h)
-            one_image_bboxes.append([xmin, ymin, xmax, ymax])
-            # f.write(text + '\n')
-        bboxes.append(one_image_bboxes)
-
-    data['images'] = images
-    data['bboxes'] = bboxes
-
-    return data
