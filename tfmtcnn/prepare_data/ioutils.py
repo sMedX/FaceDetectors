9e8ae21ddca34f27ceab0f9f52cca259271445ce
kosarev@smedx.com <kosarev@smedx.com>
2019-04-04 00:48:56 +0300

    read annotations added

--- tfmtcnn/prepare_data/ioutils.py
+++ tfmtcnn/prepare_data/ioutils.py
@@ -96,3 +96,49 @@ class ImageLoaderWithPath:
     def reset(self):
         self.counter = 0
         return self
+
+
+def read_annotation(base_dir, label_path):
+    """
+    read label file
+    :param dir: path
+    :return:
+    """
+    data = dict()
+    images = []
+    bboxes = []
+    file = label_path.open('r')
+
+    while True:
+        # image path
+        imagepath = file.readline().strip('\n')
+        if not imagepath:
+            break
+        imagepath = base_dir.joinpath(imagepath)
+        images.append(imagepath)
+        # face numbers
+        nums = file.readline().strip('\n')
+        # im = cv2.imread(imagepath)
+        # h, w, c = im.shape
+        one_image_bboxes = []
+        for i in range(int(nums)):
+            # text = ''
+            # text = text + imagepath
+            bb_info = file.readline().strip('\n').split(' ')
+            # only need x, y, w, h
+            face_box = [float(bb_info[i]) for i in range(4)]
+            # text = text + ' ' + str(face_box[0] / w) + ' ' + str(face_box[1] / h)
+            xmin = face_box[0]
+            ymin = face_box[1]
+            xmax = xmin + face_box[2]
+            ymax = ymin + face_box[3]
+            # text = text + ' ' + str(xmax / w) + ' ' + str(ymax / h)
+            one_image_bboxes.append([xmin, ymin, xmax, ymax])
+            # f.write(text + '\n')
+        bboxes.append(one_image_bboxes)
+
+
+    data['images'] = images#all images
+    data['bboxes'] = bboxes#all image bboxes
+    # f.close()
+    return data
