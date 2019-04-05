1411d82a7496eaa43c49f5a8c6b943361839d892
kosarev@smedx.com <kosarev@smedx.com>
2019-04-05 11:10:22 +0300

    generation examples has been updated

--- tfmtcnn/prepare_data/wider.py
+++ tfmtcnn/prepare_data/wider.py
@@ -176,6 +176,128 @@ class DBWider:
                         sample = [-1, offset_x1, offset_y1, offset_x2, offset_y2]
                         self.add_to_tfrecord('part', resized, sample)
 
+    def hardexamples(self, configs, threshold=(0.6, 0.7, 0.7), min_face_size=20, stride=2):
+
+        if not len(configs) in (2, 3):
+            raise ValueError('the number of configs must be 2 or 3.')
+
+        tfprefix = configs[len(configs)-1].dbase.tfprefix
+        ioutils.mkdir(tfprefix.parent)
+
+        def writer(label):
+            tffile = tfrecords.getfilename(tfprefix, label)
+            return tf.python_io.TFRecordWriter(str(tffile))
+
+        with contextlib.ExitStack() as stack:
+            self.tfwriter = {label: stack.enter_context(writer(label)) for label in self.labels}
+            self._hardexamples(configs, threshold, min_face_size, stride)
+
+    def _hardexamples(self, configs, threshold, min_face_size, stride):
+        from tfmtcnn.models import pnet
+        from tfmtcnn.models import rnet
+        from tfmtcnn.mtcnn import MTCNN
+        from tfmtcnn.prepare_data.utils import convert_to_square
+
+        detectors = [None, None, None]
+
+        batch_size = 256
+
+        for i, (config, net) in enumerate(zip(configs, (pnet.PNet, rnet.RNet))):
+            if i < len(configs)-1:
+                model_path = '{}-{}'.format(config.prefix, config.number_of_epochs)
+                detectors[i] = net(config, batch_size, model_path)
+            else:
+                image_size = config.image_size
+
+        # load P-Net model (detectors[0]) to generate data to train R-Net or O-Net
+        # config = configs[0]
+        # model_path = '{}-{}'.format(config.prefix, config.number_of_epochs)
+        # detectors[0] = pnet.PNet(config, model_path)
+        # image_size = configs[1].image_size
+        # if len(configs) > 2:
+        #     # load R-Net model (detectors[1]) to generate data to train O-Net
+        #     config = configs[1]
+        #     model_path = '{}-{}'.format(config.prefix, config.number_of_epochs)
+        #     detectors[1] = rnet.RNet(config, batch_size, model_path)
+        #     image_size = configs[2].image_size
+
+        # initialize detector
+        detector = MTCNN(detectors=detectors,
+                         min_face_size=min_face_size,
+                         stride=stride,
+                         threshold=threshold)
+
+        data = ioutils.read_annotation(self.images, self.wider_face_train_bbx_gt)
+        # data['images'] = data['images'][:500]
+        # data['bboxes'] = data['bboxes'][:500]
+
+        # index of negative, positive and part face, used as their image names
+        # positive = []
+        # negative = []
+        # part = []
+
+        loader = ioutils.ImageLoader(data['images'], prefix=self.images)
+
+        for img, gts in zip(loader, data['bboxes']):
+            dets, _ = detector.detect(img)
+            if dets.shape[0] == 0:
+                continue
+
+            # change to square
+            dets = convert_to_square(dets)
+            dets[:, 0:4] = np.round(dets[:, 0:4])
+
+            gts = np.array(gts, dtype=np.float32).reshape(-1, 4)
+
+            neg_num = 0
+
+            for box in dets:
+                x_left, y_top, x_right, y_bottom, _ = box.astype(int)
+                width = x_right - x_left + 1
+                height = y_bottom - y_top + 1
+
+                # ignore box that is too small or beyond image border
+                if width < 20 or x_left < 0 or y_top < 0 or x_right > img.shape[1] - 1 or y_bottom > img.shape[0] - 1:
+                    continue
+
+                # compute intersection over union(IoU) between current box and all gt boxes
+                iou_values = IoU(box, gts)
+
+                cropped = img[y_top:y_bottom + 1, x_left:x_right + 1, :]
+                resized = cv2.resize(cropped, (image_size, image_size), interpolation=cv2.INTER_LINEAR)
+
+                # save negative images and write label Iou with all gts must below 0.3
+                if np.max(iou_values) < 0.3 and neg_num < 60:
+                    # key_name = os.path.join('negative', '{}.jpg'.format(len(negative)))
+                    # ioutils.write_image(resized, key_name, prefix=h5file.parent)
+                    # negative.append((key_name, 0, 0, 0, 0, 0))
+                    self.add_to_tfrecord('negative', resized, [0, 0, 0, 0, 0])
+                    neg_num += 1
+                else:
+                    # find gt_box with the highest iou
+                    idx = np.argmax(iou_values)
+                    assigned_gt = gts[idx]
+                    x1, y1, x2, y2 = assigned_gt
+
+                    # compute bbox reg label
+                    offset_x1 = (x1 - x_left) / float(width)
+                    offset_y1 = (y1 - y_top) / float(height)
+                    offset_x2 = (x2 - x_right) / float(width)
+                    offset_y2 = (y2 - y_bottom) / float(height)
+
+                    # save positive and part-face images and write labels
+                    if np.max(iou_values) >= 0.65:
+                        # key_name = os.path.join('positive', '{}.jpg'.format(len(positive)))
+                        # ioutils.write_image(resized, key_name, prefix=h5file.parent)
+                        # positive.append((key_name, 1, offset_x1, offset_y1, offset_x2, offset_y2))
+                        self.add_to_tfrecord('positive', resized, [1, offset_x1, offset_y1, offset_x2, offset_y2])
+
+                    elif np.max(iou_values) >= 0.4:
+                        # key_name = os.path.join('part', '{}.jpg'.format(len(part)))
+                        # ioutils.write_image(resized, key_name, prefix=h5file.parent)
+                        # part.append((key_name, -1, offset_x1, offset_y1, offset_x2, offset_y2))
+                        self.add_to_tfrecord('part', resized, [-1, offset_x1, offset_y1, offset_x2, offset_y2])
+
     def add_to_tfrecord(self, label, image, sample):
         image_buffer = image.tostring()
 
