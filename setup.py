64edf8b4044c5ec04ce20e2bc95d4f47e25dcbf7
Eugene Prilepin <esp.home@gmail.com>
2019-04-08 01:14:12 +0300

    Add setup.py script

---
+++ setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='TFMTCNN',
+    version='0.0.1',
+
+    python_requires='>=3.5',
+
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'tfmtcnn': [
+            'data/*.txt',
+            'images/*.jpg',
+        ],
+        'tfmtcnn.models': [
+            'parameters/*/*'
+        ]
+    },
+
+    install_requires=[
+        'Click >= 7.0',
+        'numpy >= 1.15.0',
+        'h5py >= 2.9.0',
+        'opencv-python >= 3.1.0.0, <= 3.4.5.20',
+        'tensorflow >= 1.13.1',
+        'tensorboard >= 1.13.1',
+    ],
+
+    extras_require={
+        'GPU': ['tensorflow_gpu >= 1.13.1'],
+    },
+
+    entry_points={
+        'console_scripts': [
+            'mtcnn_example = tfmtcnn.apps.example:main',
+            'train_mtcnn = tfmtcnn.apps.train_mtcnn:main',
+            'lfw_test = tfmtcnn.apps.lfw_test:main',
+            'lfw_pypi_test = tfmtcnn.apps.lfw_pypi_test:main',
+        ],
+    },
+
+    url='https://github.com/RuslanKosarev/TFMTCNN',
+    license='MIT',
+    author='Ruslan Kosarev',
+    author_email='ruslan.kosarev@gmail.com',
+
+    description=(
+        'MTCNN, a joint face detection and alignment using multi-task '
+        'cascaded convolutional networks'
+    ),
+)
