1882bc1456237bb4dfefc31e563b928f0bc89984
korus <ruslan.kosarev@gmail.com>
2020-05-13 18:43:15 +0300

    update def detector

--- face_detection/config.py
+++ face_detection/config.py
@@ -7,5 +7,5 @@ dir_images = data_dir.joinpath('images')
 dir_output = data_dir.joinpath('output')
 
 # detectors
-default_detector = 'ssdlite_mobilenet'
+default_detector = 'ssdlite_mobilenet_v2_coco_2018_05_09'
 
